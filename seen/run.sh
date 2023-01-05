8843053cb302587d292daa8b9312f8dda758db42
githubjacky <opottghjk00@gmail.com>
2023-01-05 20:30:00 +0800

    seen, update requirements.txt, run.sh

--- seen/run.sh
+++ seen/run.sh
@@ -1,1 +1,9 @@
-pthon seen.py "${1}"
+# download data
+if [[ ! -d "data" ]]; then
+    wget -O data.zip https://www.dropbox.com/s/a7y1592mpg4miyp/data.zip?dl=0
+    unzip data.zip
+    rm data.zip
+fi
+
+python seen.py "${1}"
+
