f63e700802f068b5be0d2a602e6e942711d10344
Grand-ou <ouchungkai@gmail.com>
2023-01-05 23:12:26 +0800

    drop

--- unseen/topic/download.sh
+++ unseen/topic/download.sh
@@ -6,7 +6,7 @@ unzip 7.zip
 =======
 #!/bin/bash
 wget https://www.dropbox.com/s/p0u8ew1xgw9fu8n/7.zip
-wget https://www.dropbox.com/s/9w4gdcapxkqnyzc/data.zip
+wget https://www.dropbox.com/s/b5wnae6usdgtkeb/data.zip
 unzip 7.zip
 >>>>>>> 5e1a5dc (dropbox)
 unzip data.zip
