0ff261efdc684fb6ca884b0a1e07a61ef33e8f0d
Grand-ou <65006555+Grand-ou@users.noreply.github.com>
2023-01-05 23:32:31 +0800

    Update download.sh

--- unseen/topic/download.sh
+++ unseen/topic/download.sh
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 #!/bin/bash
 wget https://www.dropbox.com/s/p0u8ew1xgw9fu8n/7.zip
 wget https://www.dropbox.com/s/b5wnae6usdgtkeb/data.zip
 unzip 7.zip
-=======
-#!/bin/bash
-wget https://www.dropbox.com/s/p0u8ew1xgw9fu8n/7.zip
-wget https://www.dropbox.com/s/b5wnae6usdgtkeb/data.zip
-unzip 7.zip
->>>>>>> 5e1a5dc (dropbox)
 unzip data.zip
